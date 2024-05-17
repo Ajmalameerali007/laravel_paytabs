f9c1a3e4890e7bf4be7e83eba75475445f78c2d8
Ajmalameerali007 <ajmalameerali@gmail.com>
2024-05-17 04:11:55 +0400

    Main

--- config/config.php
+++ config/config.php
@@ -1,49 +1,9 @@
 <?php
 
 /*
- * This file is part of paytabs.
- *
- * (c) Walaa Elsaeed <w.elsaeed@paytabs.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
 
-return [
 
-    /*
-     |--------------------------------------------------------------------------
-     | Merchant profile id
-     |--------------------------------------------------------------------------
-     |
-     | Your merchant profile id , you can find the profile id on your PayTabs Merchant’s Dashboard- profile.
-     |
-     */
-
-    'profile_id' => env('paytabs_profile_id', null),
-
-    /*
-   |--------------------------------------------------------------------------
-   | Server Key
-   |--------------------------------------------------------------------------
-   |
-   | You can find the Server key on your PayTabs Merchant’s Dashboard - Developers - Key management.
-   |
-   */
-
-    'server_key' => env('paytabs_server_key', null),
-
-    /*
-   |--------------------------------------------------------------------------
-   | Currency
-   |--------------------------------------------------------------------------
-   |
-   | The currency you registered in with PayTabs account
-     you must pass value from this array ['AED','EGP','SAR','OMR','JOD','US']
-   |
-   */
-
-    'currency' => env('paytabs_currency', null),
+    'currency' => env(AED', null),
 
 
     /*
@@ -56,6 +16,6 @@ return [
    |
    */
 
-    'region' => env('paytabs_region', null),
+    'region' => env('AE', null),
 
 ];
