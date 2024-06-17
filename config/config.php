0f9270d11efb4d43ca60dbc79ae15d1a8b4c02f4
Ajmalameerali007 <137335517+Ajmalameerali007@users.noreply.github.com>
2024-06-17 05:02:36 +0000

    chore: Update PayTabs configuration for currency and region

--- config/config.php
+++ config/config.php
@@ -1,21 +1,8 @@
 <?php
+[
 
-/*
+    'currency' => 'aed',
 
+    'region' => 'ARE',
 
-    'currency' => env(AED', null),
-
-
-    /*
-   |--------------------------------------------------------------------------
-   | Region
-   |--------------------------------------------------------------------------
-   |
-   | The region you registered in with PayTabs
-     you must pass value from this array ['ARE','EGY','SAU','OMN','JOR','GLOBAL']
-   |
-   */
-
-    'region' => env('AE', null),
-
-];
+]
--- src/config/config.php
+++ src/config/config.php
@@ -1,61 +1,12 @@
 <?php
+[
 
-/*
- * This file is part of paytabs.
- *
- * (c) Walaa Elsaeed <w.elsaeed@paytabs.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+    'profile_id' => '142466',
 
-return [
+    'server_key' => 'SHJ9RGHJZN-JJHMRRG6BM-NJ6KJJK2ND',
 
-    /*
-     |--------------------------------------------------------------------------
-     | Merchant profile id
-     |--------------------------------------------------------------------------
-     |
-     | Your merchant profile id , you can find the profile id on your PayTabs Merchant’s Dashboard- profile.
-     |
-     */
+    'currency' => 'aed',
 
-    'profile_id' => env('paytabs_profile_id', null),
+    'region' => 'UAE',
 
-    /*
-   |--------------------------------------------------------------------------
-   | Server Key
-   |--------------------------------------------------------------------------
-   |
-   | You can find the Server key on your PayTabs Merchant’s Dashboard - Developers - Key management.
-   |
-   */
-
-    'server_key' => env('paytabs_server_key', null),
-
-    /*
-   |--------------------------------------------------------------------------
-   | Currency
-   |--------------------------------------------------------------------------
-   |
-   | The currency you registered in with PayTabs account
-     you must pass value from this array ['AED','EGP','SAR','OMR','JOD','US']
-   |
-   */
-
-    'currency' => env('paytabs_currency', null),
-
-
-    /*
-   |--------------------------------------------------------------------------
-   | Region
-   |--------------------------------------------------------------------------
-   |
-   | The region you registered in with PayTabs
-     you must pass value from this array ['ARE','EGY','SAU','OMN','JOR','GLOBAL']
-   |
-   */
-
-    'region' => env('paytabs_region', null),
-
-];
+]
