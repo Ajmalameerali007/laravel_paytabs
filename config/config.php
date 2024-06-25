746413df30f917f622181b0bd221addb3a107d9a
Ajmalameerali007 <137335517+Ajmalameerali007@users.noreply.github.com>
2024-06-25 04:34:50 +0400

    main

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<<<<<<< HEAD
+<?php
+$array = [
+    'currency' => 'aed',
+    'egion' => 'ARE',
+    "profileId" => "142466",
+    "merchantId" => "82709",
+    "merchantName" => "IFIT FITNESS CLUB LLC"
+];
+=======
+
+>>>>>>> 8d8fd28f0648dc75d1afa24b161067ddd8a30425
